d4358a12bfdd656f137fbf40051931315336c57e
Hans Donner <hans.donner@pobox.com>
2012-07-19 18:12:28 +0200

    Add commandline arguments backtrace & formatter

--- extensions/command/Phpspec.php
+++ extensions/command/Phpspec.php
@@ -4,8 +4,27 @@ namespace li3_phpspec\extensions\command;
 
 class Phpspec extends \lithium\console\Command {
 
+    /**
+     * Enable displaying the backtrace on errors
+     */
+    public $backtrace = false;
+    
+    /**
+     * Set the format mode
+     */
+    public $formatter = null;
+
     public function run($path) {
-        $args = (array)$path;
+        if ($this->formatter === true) {
+            $this->formatter = false;
+        };
+        $args = array(
+            $path,
+            $this->backtrace ? '-b' : '',
+            $this->formatter ? '-f' : '',
+            $this->formatter ?: '',
+        );
+        array_filter($args);
         $phpspec = new \PHPSpec\PHPSpec($args);
         $phpspec->execute();
     }
